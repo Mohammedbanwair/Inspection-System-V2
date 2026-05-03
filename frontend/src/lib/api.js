196c8f563bb80b7ba36727332d07b93c3aedc06d
Mohammedbanwair <mohbanwair@gmail.com>
2026-05-03 22:42:36 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://inspection-system-v2-production.up.railway.app";
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({
